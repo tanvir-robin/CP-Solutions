d3a7429a7a14869ac5b999b7bb66415932c130be
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 03:02:15 +0600

    Committing files

---
+++ 1352C-K-th Not Divisible by n.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main()
+{
+    ll n,t,i,j,k;
+    cin >> t;
+    while (t--)
+    {
+        cin>>n>>k;
+        j= ((k - 1) / (n - 1))+k;
+        cout <<j<< endl;
+ 
+    }
+ 
+ 
+    //cout<<endl; main();
+}
