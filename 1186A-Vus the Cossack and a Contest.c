dd23d1169a999aade377e471688438cd49032d14
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 03:15:41 +0600

    Committing files

---
+++ 1186A-Vus the Cossack and a Contest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main()
+{ int n,m,p;
+  scanf("%d %d %d",&n,&m,&p);
+ 
+  if(m>=n && p>=n)
+    printf("Yes");
+  else printf("No");
+ 
+ 
+}
