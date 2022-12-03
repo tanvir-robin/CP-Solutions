d08aec28884c2f382f8d35d319e1b9ea6246ef96
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 03:02:57 +0600

    Committing files

---
+++ 1663B-Mike's Sequence.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main()
+{
+    ll n,t,i,j,k;
+    cin>>n;
+    if(n<1200) cout<<1200<<endl;
+   else if(n<1400) cout<<1400;
+   else if(n<1600) cout<<1600;
+   else if(n<1900) cout<<1900;
+   else if(n<2100) cout<<2100;
+   else if(n<2300) cout<<2300;
+   else if(n<2400) cout<<2400;
+   else if(n<2600) cout<<2600;
+   else cout<<3000;
+ 
+  //cout<<endl; main();
+}
+ 
