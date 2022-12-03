1c97fae92223d9190daf86e8b614b34c6c1d5ec7
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 03:04:35 +0600

    Committing files

---
+++ 1749A-Cowardly Rooks.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long tst,m,n,t,tmp,a,b;
+    cin>>tst;
+    while(tst--)
+    {
+        cin>>n>>m;
+        tmp=m;
+        while(m--)
+        {
+            cin>>a>>b;
+        }
+        if(tmp<n)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+ 
+}
+ 
