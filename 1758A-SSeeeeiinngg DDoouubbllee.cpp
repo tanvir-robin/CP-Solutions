7a559ce756577e5e550cae26f4a46404c58b0220
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 02:59:37 +0600

    Committing files

---
+++ 1758A-SSeeeeiinngg DDoouubbllee.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define all(x) x.begin(),x.end()
+#define pb push_back
+//--------------------------------------------------------------
+ 
+int main()
+{
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    bool flag=false;
+    string s,s1,s2,s3;
+ 
+    cin>>t;
+ 
+    while(t--) {
+        cin>>s;
+        cout<<s;
+        reverse(all(s));
+        cout<<s;
+        nl;
+ 
+    }
+ 
+  //cout<<endl; main();
+}
+ 
+ 
