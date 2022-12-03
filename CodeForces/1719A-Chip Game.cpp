58f1e423afa716ca227c5daed0ebaa17a19e4511
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 02:56:17 +0600

    Committing files

---
+++ CodeForces/1719A-Chip Game.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define DouraMama {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+ 
+int main()
+{   DouraMama
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    bool flag=false;
+    string s,s1,s2,s3;
+ 
+    cin>>t;
+ 
+    while(t--) {
+         cin>>n>>m;
+         if((n%2 && m%2) || (n%2==0 && m%2==0)) cout<<"Tonya"<<endl;
+         else cout<<"Burenka"<<endl;
+ 
+    }
+ 
+  //cout<<endl; main();
+}
+ 
+ 
