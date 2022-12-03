d508fa55bc7234f6d184c5f062dac54d794c9cd9
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 03:02:23 +0600

    Committing files

---
+++ 460A-Vasya and Socks.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main()
+{
+    ll n,t,i,j,k,m,ans=0;
+    cin>>n>>m;
+    k=(n-1)/(m-1);
+    ans+=n+k; cout<<ans;
+ 
+  //cout<<endl; main();
+}
+ 
