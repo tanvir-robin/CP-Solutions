35ec24143004b0292c87df4d062f072ef602c184
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 03:03:55 +0600

    Committing files

---
+++ 1743B-Permutation Value.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    
+   
+     int t,i,j,k,n;
+    cin>>t;
+    while(t--) {
+         cin>>n;
+        for(i=2; i<=n; i++)
+           cout<<i<<" ";
+           cout<<1<<endl;
+    
+    }
+}
