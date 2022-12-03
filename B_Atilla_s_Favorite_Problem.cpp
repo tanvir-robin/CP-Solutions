3b7811c6b9a7c5ec518d90e424e345aa06c80ba9
Tanvir Islam Robin <tanvir.rrrabin@gmail.com>
2022-12-04 00:16:20 +0600

    B_Atilla_s_Favorite_Problem

---
+++ B_Atilla_s_Favorite_Problem.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(i=x; i<y; i++)
+#define flc(i,x,y,z) for(i=x; i<y; i+=z)
+#define nl cout<<endl
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define all(x) x.begin(),x.end()
+#define pb push_back
+//--------------------------------------------------------------
+
+int main()
+{
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    bool flag=false;
+    string s,s1,s2,s3;
+
+    cin>>t;
+
+    while(t--) {
+        cin>>n>>s;
+        vector<char> v;
+        ans=s[0];
+        fl(i,0,s.size()) {
+           if(s[i]>ans) ans=s[i];
+        }
+        printf("%d\n",ans-96);
+
+
+    }
+
+  //cout<<endl; main();
+}
+
+
