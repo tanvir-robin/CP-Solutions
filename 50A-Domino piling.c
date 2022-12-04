0d3d2e13922294de7267772241cd8df6f25dd3e8
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 12:17:22 +0600

    Committing files

---
+++ 50A-Domino piling.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<ctype.h>
+ 
+int main()
+{
+   int m,n,res;
+   scanf("%d %d",&m,&n);
+ 
+   res = (m*n)/2;
+   printf("%d",res);
+}
