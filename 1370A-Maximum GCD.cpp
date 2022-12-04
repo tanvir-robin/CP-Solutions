386f4d1bf94995b81fec2a05b67a3e86f9201fbf
Tanvir Islam Robin <95021955+RooBiiinnn@users.noreply.github.com>
2022-12-04 12:16:54 +0600

    Committing files

---
+++ 1370A-Maximum GCD.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ 
+int main()
+{
+    int t,n1,n2,gc,rem,i,j,k,l,m;
+    scanf("%d",&t);
+    int n[t+10];
+    //input
+    for(i=0; i<t; i++)
+        scanf("%d",&n[i]);
+ 
+ 
+    //processing
+    for(i=0; i<t; i++)
+    {   
+    gc=n[i]/2;
+    printf("\n%d",gc);
+    
+        }
+   
+}
