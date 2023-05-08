1d873b23113e35cd3931158c9d5d4839ae1cfbe6
Tanvir Islam Robin <tanvir.rrrabin@gmail.com>
2023-05-08 21:11:03 +0600

    Committing files

---
+++ 1005A-Tanya and Stairways.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(int i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define getIntoVc(v,n) { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define DouraMama {ios::sync_with_stdio(false); cin.tie(NULL);}
+#define prefixSum(v,prefix) {long long sum=0; for(int i=0; i<v.size(); i++) {sum+=v[i];prefix.push_back(sum);}}
+//--------------------------------------------------------------
+ 
+int main()
+{   DouraMama
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=1,ans=1;
+    bool flag=false;
+    string s,s1,s2,s3;
+    vector<ll> v;
+    cin>>n;
+    cin>>x; n--;
+    while(n--) {
+        cin>>x;
+        if(x==1) {
+            v.pb(cnt);
+            cnt=0;
+        }
+        cnt++;
+    }
+   // if(x!=1)
+    v.pb(cnt);
+    cout<<v.size()<<endl;
+    printvc(v);
+   
+     
+ 
+  //cout<<endl; main();
+}
+ 
+ 
