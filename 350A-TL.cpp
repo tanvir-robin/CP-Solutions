dae6bb02ec8c68c36603a2fa6502fa122c6c75e9
Tanvir Islam Robin <tanvir.rrrabin@gmail.com>
2023-05-08 21:03:26 +0600

    Committing files

---
+++ 350A-TL.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y)              for(int i=x; i<y; i++)
+#define flc(i,x,y,z)           for(int i=x; i<y; i+=z)
+#define nl                     cout<<endl
+#define vsort(v)               sort(v.begin(), v.end());
+#define vsortg(v)              sort(v.begin(), v.end(), greater<int>());
+#define getIntoVc(v,n)         { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
+#define all(x)                 x.begin(),x.end()
+#define pb                     push_back
+#define printvc(v)             for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n)          {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s)          {stringstream sst; sst<<n; sst>>s;}
+#define ff                     first
+#define ss                     second
+#define RedCoderErPaDhowaPani  {ios::sync_with_stdio(false); cin.tie(NULL);}
+#define prefixSum(v,p)         {long long sum=0; for(int i=0; i<v.size(); i++) {sum+=v[i];p.push_back(sum);}}
+//--------------------------------------------------------------
+ 
+void AladinsLamp() {
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0,res=0;
+    bool flag=false;
+    string s,s1,s2,s3;
+    vector<ll> cr;
+    vector<ll> w;
+    cin>>n>>m;
+    getIntoVc(cr,n);
+    getIntoVc(w,m);
+    vsort(cr);
+    vsort(w);
+    res=*min_element(all(cr));
+    x=*max_element(all(cr));
+    c= *min_element(all(w));
+    if(max(2*res,x)<c) cout<<max(2*res,x);
+    else cout<<-1;
+ 
+ 
+ 
+}
+int main()
+{   RedCoderErPaDhowaPani
+    AladinsLamp();
+}
+ 
+ 
