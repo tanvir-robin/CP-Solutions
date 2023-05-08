0011d42bdf5b3d38cdffb319237cf498795e3a87
Tanvir Islam Robin <tanvir.rrrabin@gmail.com>
2023-05-08 21:09:22 +0600

    Committing files

---
+++ 1658A-Marin and Photoshoot.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(int i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define getIntoVc(v,n) { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define DouraMama {ios::sync_with_stdio(false); cin.tie(NULL);}
+#define prefixSum(v,prefix) {long long sum=0; for(int i=0; i<v.size(); i++) {sum+=v[i];prefix.push_back(sum);}}
+//--------------------------------------------------------------
+ 
+int main()
+{   DouraMama
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    bool flag=false;
+    string s,s1,s2,s3;
+ 
+    cin>>t;
+ 
+    while(t--) {
+        cin>>n>>s;
+        vector<ll> v;
+        fl(i,0,s.size()) {
+            if(s[i]=='0') v.pb(i);
+        }
+        cnt=0;
+        if(v.size()!=0) {
+            fl(i,0,v.size()-1) {
+            ans=2-(v[i+1]-v[i]);
+            if(ans>=0) cnt+=ans+1;
+        }
+        }
+       cout<<cnt<<endl;
+    }
+ 
+  //cout<<endl; main();
+}
+ 
+ 
