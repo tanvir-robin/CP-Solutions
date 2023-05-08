d8432bce2947556034419b2d99613c1ecef558bd
Tanvir Islam Robin <tanvir.rrrabin@gmail.com>
2023-05-08 21:14:32 +0600

    Committing files

---
+++ 448A-Rewards.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(int i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define getIntoVc(v,n) { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define DouraMama {ios::sync_with_stdio(false); cin.tie(NULL);}
+//--------------------------------------------------------------
+ 
+int main()
+{   DouraMama
+    ll m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    float n;
+    bool flag=false;
+    string s,s1,s2,s3;
+    vector<ll> v;
+    cin>>a>>b>>c;
+    cin>>x>>y>>z;
+    cin>>n;
+    ll cups=a+b+c;
+    ll medals=x+y+z;
+    ans=ceil((cups/5.00))+ceil((medals/10.00));
+     //cout<<ans<<endl;
+    if(ans<=n) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+   
+     
+ 
+  //cout<<endl; main();
+}
+ 
+ 
