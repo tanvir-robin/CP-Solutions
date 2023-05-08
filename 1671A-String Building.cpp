317bf1c8fa063108acdee86379a041c44349fd61
Tanvir Islam Robin <tanvir.rrrabin@gmail.com>
2023-05-08 21:09:00 +0600

    Committing files

---
+++ 1671A-String Building.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(int i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define getIntoVc(v,n) { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define DouraMama {ios::sync_with_stdio(false); cin.tie(NULL);}
+#define prefixSum(v,prefix) {long long sum=0; for(int i=0; i<v.size(); i++) {sum+=v[i];prefix.push_back(sum);}}
+//--------------------------------------------------------------
+ 
+void solve() {
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    bool flag=false;
+    string s,s1,s2,s3;
+     cin>>s;
+     if(s.size()==1) {
+          cout<<"NO"<<endl; return;;
+     }
+      fl(i,0,s.size()) {
+        if(i!=0) {
+            if(!(s[i]==s[i+1] || s[i]==s[i-1])) {
+                cout<<"NO"<<endl; return;;
+            } 
+        }
+        else {
+            if(s[i]!=s[i+1]) {
+                cout<<"NO"<<endl; return;
+            }
+        }
+      }
+      cout<<"YES"<<endl;
+}
+ 
+int main()
+{   DouraMama
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    bool flag=false;
+    string s,s1,s2,s3;
+ 
+    cin>>t;
+ 
+    while(t--) {
+     solve();
+ 
+    }
+ 
+  //cout<<endl; main();
+}
+ 
+ 
