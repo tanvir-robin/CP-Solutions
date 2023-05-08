630598518d6517b6511415b966c8f9509c807fc5
Tanvir Islam Robin <tanvir.rrrabin@gmail.com>
2023-05-08 21:04:55 +0600

    Committing files

---
+++ 118B-Present from Lena.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y)              for(int i=x; i<y; i++)
+#define flc(i,x,y,z)           for(int i=x; i<y; i+=z)
+#define nl                     cout<<endl
+#define vsort(v)               sort(v.begin(), v.end());
+#define vsortg(v)              sort(v.begin(), v.end(), greater<int>());
+#define getIntoVc(v,n)         { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
+#define all(x)                 x.begin(),x.end()
+#define pb                     push_back
+#define printvc(v)             for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n)          {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s)          {stringstream sst; sst<<n; sst>>s;}
+#define ff                     first
+#define ss                     second
+#define RedCoderErPaDhowaPani  {ios::sync_with_stdio(false); cin.tie(NULL);}
+#define prefixSum(v,p)         {long long sum=0; for(int i=0; i<v.size(); i++) {sum+=v[i];p.push_back(sum);}}
+//--------------------------------------------------------------
+ 
+int main()
+{   RedCoderErPaDhowaPani
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0,res=0;
+    bool flag=false;
+    string s,s1,s2,s3;
+    vector<ll> v;
+    cin>>n;
+ 
+    fl(i,0,n+1)
+    {
+       fl(j,0,n-i)      cout<<"  ";
+ 
+       fl(j,0,i+1)
+        {
+            cout<<j;
+            if(j!=i)    cout<<" ";
+  
+        }
+        for(int j=i-1; j>=0; j--)
+        {
+            cout<<" ";
+            cout<<j;
+        }
+       nl;
+    }
+    for(int i=n-1; i>=0; i--)
+    {
+        fl(j,0,n-i)    cout<<"  ";
+ 
+       fl(j,0,i+1)
+        {
+            cout<<j;
+            if(j!=i)    cout<<" ";
+            
+        }
+        for(int j=i-1; j>=0; j--)
+        {
+            cout<<" ";
+            cout<<j;
+        }
+        nl;
+    }
+   
+       //cout<<endl; main();
+}
+ 
+ 
