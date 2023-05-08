4bbc2199494e2fb3357afde63e33fea93a797881
Tanvir Islam Robin <tanvir.rrrabin@gmail.com>
2023-05-08 21:08:13 +0600

    Committing files

---
+++ 1657A-Integer Moves.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+//Definition Section
+//--------------------------------------------------------------
+#define fl(i,x,y) for(int i=x; i<y; i++)
+#define flc(i,x,y,z) for(int i=x; i<y; i+=z)
+#define nl cout<<endl
+#define vsort(v) sort(v.begin(), v.end());
+#define vsortg(v) sort(v.begin(), v.end(), greater<int>());
+#define getIntoVc(v,n) { while(n--) {long long yyy; cin>>yyy; v.push_back(yyy);}}
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define printvc(v) for(int i=0; i<v.size(); i++) cout<<v[i]<<" ";
+#define strTOint(s,n) {stringstream sst; sst<<s; sst>>n;}
+#define intTOstr(n,s) {stringstream sst; sst<<n; sst>>s;}
+#define ff first
+#define ss second
+#define DouraMama {ios::sync_with_stdio(false); cin.tie(NULL);}
+#define prefixSum(v,prefix) {long long sum=0; for(int i=0; i<v.size(); i++) {sum+=v[i];prefix.push_back(sum);}}
+//--------------------------------------------------------------
+ 
+int main()
+{   DouraMama
+    ll n,m,t,i,j,k,x,y,z,a,b,c,cnt=0,ans=0;
+    bool flag=false;
+    string s,s1,s2,s3;
+ 
+    cin>>t;
+ 
+    while(t--) {
+      cin>>x>>y;
+      if(x==0 && y==0) {cout<<0<<endl; continue;}
+      ans=(x*x)+(y*y);
+      cnt=floor(sqrt(ans));
+      if(cnt*cnt==ans) cout<<1<<endl;
+      else cout<<2<<endl;
+ 
+    }
+ 
+  //cout<<endl; main();
+}
+ 
+ 
